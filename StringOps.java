84dfb753e377a993ea574d3302ccd6833daa5c10
Yuvalabiri <154335961+Yuvalabiri@users.noreply.github.com>
2024-01-17 21:01:36 +0200

    yuval_abiris_awesome_code4test

--- StringOps.java
+++ StringOps.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringOps {
     ////////////////////////////////////////////////////////////
     //////                                               ///////
@@ -23,20 +25,91 @@ public class StringOps {
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
         
+        
     }
 
-    public static String capVowelsLowRest (String string) {
+    public static String capVowelsLowRest (String str) {
         // Write your code here:
-        return "";
+        String res = "";
+        char [] vowels = {'a', 'e', 'i', 'o', 'u'};
+        for (int i = 0; i < str.length(); i++) {
+            char chr = str.charAt(i);
+            for (int j = 0; j < vowels.length; j++){
+                if (chr == vowels[j]) {
+                    chr = (char)(chr - 32); 
+                    break;
+                }
+            }
+            if (chr > 65 && chr <= 90 && chr != 69 && chr != 73 && chr != 79 && chr != 85) {
+                chr = (char)(chr + 32);
+            }
+            res = res + chr;
+        }   
+        return res;
     }
 
-    public static String camelCase (String string) {
+    public static String camelCase (String str) {
         // Write your code here:
-        return "";
+        boolean nexttoupper = false;
+        String res = "";
+        char temp; 
+        boolean firstword = false;
+
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == ' ') {
+                nexttoupper = true;
+                continue;
+            }
+            if (nexttoupper == true && firstword == true) {
+                temp = makeitupper(str.charAt(i));
+            } else {
+                temp = makeitlower(str.charAt(i));
+            }
+            res += temp;
+            firstword = true;
+            nexttoupper = false;
+        }
+        return res;
     }
 
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+    public static char makeitupper (char chr) {
+        if (chr >= 97 && chr <= 122 ) {
+            chr = (char)(chr - 32);
+        }
+        return chr;
     }
+
+    public static char makeitlower (char chr) {
+        if (chr >= 65 && chr <= 90) {
+            chr = (char)(chr + 32);
+        }
+        return chr;
+    }
+
+
+    public static int[] allIndexOf (String str, char chr) {
+        // Write your code here:
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == chr) {
+                count++;
+            }
+        }
+
+        int[] index = new int[count];
+        int currentindex = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == chr) {
+                index[currentindex] = i;
+                currentindex++;
+            }
+        }
+
+        return index;
+    }
+  
+    
 }
+
